ff40be86fa91a331cda2dcc2d8591ae20c1f9f2b
Qwarkeh <christine.f.stark@gmail.com~>
2022-05-12 16:20:32 -0400

    Adds unique labID for current date to a list

--- PythonScripts/Loinc_queries.py
+++ PythonScripts/Loinc_queries.py
@@ -6,11 +6,25 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'Database=GME468;'
                       'Trusted_Connection=yes;')
 
-#
-cursor = conn.cursor()
+#Test Cursor
+""" cursor = conn.cursor()
 cursor.execute('SELECT * FROM Laboratories')
 
 for i in cursor:
     print(i)
+
+cursor.close() """
+
+# Populate list with unique LabID for current date
+cursor = conn.cursor()
+cursor.execute('SELECT LabID FROM LoincTransactions WHERE CAST(TransDate as DATE) = CAST(GETDATE() as DATE)')
+
+unique_lab_list = []
+for lab in cursor:
+    if lab[0] not in unique_lab_list:
+        unique_lab_list.append(lab[0])
+
+for lab in unique_lab_list:
+    print(lab)
 
 cursor.close()
