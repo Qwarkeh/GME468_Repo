de6b69f3d734fb2c300835af94e5b17fe0139286
Qwarkeh <christine.f.stark@gmail.com~>
2022-05-12 16:23:32 -0400

    Removes test cursor

--- PythonScripts/Loinc_queries.py
+++ PythonScripts/Loinc_queries.py
@@ -6,15 +6,6 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'Database=GME468;'
                       'Trusted_Connection=yes;')
 
-#Test Cursor
-""" cursor = conn.cursor()
-cursor.execute('SELECT * FROM Laboratories')
-
-for i in cursor:
-    print(i)
-
-cursor.close() """
-
 # Populate list with unique LabID for current date
 cursor = conn.cursor()
 cursor.execute('SELECT LabID FROM LoincTransactions WHERE CAST(TransDate as DATE) = CAST(GETDATE() as DATE)')
